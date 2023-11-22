7769ee6f4222abf2c11ce26f764e9f49cc3e2f4c
Bagus Kurnia <bagus.kurnia@sirclo.com>
2023-11-22 22:11:48 +0700

    add endpoint post delete update in destinations & add checkout endpoint

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const objectCar = {
+    color: "blue",
+    seat: 7,
+    brand: "toyota",
+    year: 2022
+  }
+
   res.status(200).json({
     success: true,
-    message: "Hello from bandung"
+    message: 'hello world',
+    data: objectCar
   });
 });
 
-app.get("/destinations", (req, res, next) => {
+app.post('/checkout', (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: 'destination created!'
+  });
+})
+
+app.get('/destinations', (req, res) => {
   res.status(200).json({
     success: true,
-    data: "bandung, semarang, jogja, medan, papua"
-  })
+    message: 'hello world',
+    data: "bandung lampung jakarta tangerang"
+  });
 });
+
+app.post('/destinations', (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: 'destination created!'
+  });
+})
+
+app.put('/destinations', (req, res) => {
+  console.log(req.body);
+  res.status(200).json({
+    success: true,
+    message: 'destination updated!'
+  });
+})
+
+app.delete('/destinations', (req, res) => {
+  console.log(req.body);
+  res.status(200).json({
+    success: true,
+    message: 'destination deleted!'
+  });
+})
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
